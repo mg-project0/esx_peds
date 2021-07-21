0b743a041f91c6212bc9cda9265d7b6bbb301c14
mg-project <86503049+mg-project0@users.noreply.github.com>
2021-07-21 21:45:00 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -11,7 +11,11 @@ RegisterNetEvent('esx_peds:buyItem')
 AddEventHandler('esx_peds:buyItem', function(item, price)
     local xPlayer = ESX.GetPlayerFromId(source)
     if xPlayer.getMoney() < price then TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas assez d'~r~argent") return end
-    if xPlayer.getInventoryItem(item).count + 1 >= xPlayer.getInventoryItem(item).limit and not xPlayer.getInventoryItem(item).limit == -1 then TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas assez de place dans votre ~r~inventaire") return end
+    if xPlayer.getInventoryItem(item).limit == -1 then
+        xPlayer.removeMoney(price)
+        xPlayer.addInventoryItem(item, 1)
+        TriggerClientEvent('esx:showNotification', source, "Vous venez d'acheter un ~g~"..xPlayer.getInventoryItem(item).label)
+    elseif xPlayer.getInventoryItem(item).count + 1 > xPlayer.getInventoryItem(item).limit then TriggerClientEvent('esx:showNotification', source, "Vous n'avez plus assez de place dans votre ~r~inventaire") return end
     xPlayer.removeMoney(price)
     xPlayer.addInventoryItem(item, 1)
     TriggerClientEvent('esx:showNotification', source, "Vous venez d'acheter un ~g~"..xPlayer.getInventoryItem(item).label)
